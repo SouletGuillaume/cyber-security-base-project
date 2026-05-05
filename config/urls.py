ab05a6fa0eb7211a6ebc6823720fa32e4fbb5e76
Guillaume <gsoulet36@gmail.com>
2026-05-05 13:22:49 +0200

    broken access control flaw implementation

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
+    path('account/<int:account_id>/', views.account_details, name='account_details'), # this is for the account details, used to illustrate the third flaw (broken access control)
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import UserBank
 from decimal import Decimal
@@ -43,3 +43,21 @@ def home(request):
             print("invalid number or iban format, transfer failed")
 
     return render(request, 'core/index.html', {'balance': account.balance})
+
+
+@login_required
+def account_details(request, account_id):
+
+    # FLAW 3; Broken Access Control
+    # The application fetches the account using only the ID provided in the URL.
+    # It does not verify if the currently authenticated user actually owns this account, so this allows users to access other users' accounts by simply changing the ID in the URL.
+    account = UserBank.objects.get(id=account_id)
+    
+    # FIX 3
+    # To fix this, we must enforce access control by checking the ownership of the resource.
+    # We query the database to ensure both the account ID AND the user match the logged-in user.
+
+    # Uncomment the line below and comment the vulnerable line above to apply the fix:
+    #account = get_object_or_404(UserBank, id=account_id, user=request.user)
+    
+    return render(request, 'core/account_details.html', {'account': account})
